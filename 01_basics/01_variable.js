7a9555f7d328a81dde44dc6de20ad3bbde699609
Arvind Singh <145861795+04arvind@users.noreply.github.com>
2025-03-09 06:17:26 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 14453;
+let accountEmail = "arving@gmail.com"
+let accountPassword = "641313";
+accountCity = "New Delhi";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
