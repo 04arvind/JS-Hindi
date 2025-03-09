8909a0e8857cc9cee67ed6aa875b22749ac81740
Arvind Singh <145861795+04arvind@users.noreply.github.com>
2025-03-09 06:19:47 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"  // Treat all JS code as newer version
+
+// alert("Hello")  // we are using NODE.js not browser
+
+console.log("Hello World");
+console.log(3+3)
+
+// null => stanalone value
+// symbol => unique 
+
+// object 
+console.log(typeof("Arvind"));
+console.log(typeof null);
+console.log(typeof undefined);
