21f373e70d7f20c37acf3865b89988d9f5584b61
04arvind <arvind42005@gmail.com>
2025-03-13 22:05:29 +0530

    todays another commit

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,8 @@
+const course = {
+    coursename:"Js in Hindi",
+    price: "999",
+    courseInstructor: "Arry"
+}
+// course.courseInstructor
+const {courseInstructor:instructor} = course
+console.log(instructor);  //console.log(courseInstructor)
