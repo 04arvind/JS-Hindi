d1e70f18a4486084ae3a0c0a7c1ffdc0e20d1b58
04arvind <arvind42005@gmail.com>
2025-03-14 13:18:28 +0530

    commit scopes

--- 03_basics/02_function.js
+++ 03_basics/02_function.js
@@ -1,4 +1,24 @@
+// function calculateCartPrice(...num1){
 function calculateCartPrice(val1,val2, ...num1){
     return num1;
 }
-console.log(calculateCartPrice(200,400,500, 2000))
+// console.log(calculateCartPrice(200,400,500, 2000))
+
+const user ={
+    usernmae : "Arvind",
+    prices : 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.usernmae} and price is ${anyobject.price}`)
+}
+// handleObject(user);
+handleObject({
+    usernmae:"Sam",
+    price:399
+})
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+console.log(returnSecondValue(myNewArray));
