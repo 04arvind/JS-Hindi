c7dc32a5d8a42adced9a344b65ae53c0aea9327d
04arvind <arvind42005@gmail.com>
2025-03-21 12:01:05 +0530

    project 2 is done

--- 07_Projects/2_bmicalculator/chaiaurcode.js
+++ 07_Projects/2_bmicalculator/chaiaurcode.js
@@ -1,49 +1,3 @@
-#Orihects related to DOM
-
-## Project Link :
-
-[click here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-# Solution code
-
-## Project 1
-
-```Javascript
-// console.log("Arvind")
-
-const buttons = document.querySelectorAll('.button');
-// console.log(buttons);
-const body = document.querySelector('body');
-
-buttons.forEach(function (button) {
-  console.log(button);
-  button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
-    if (e.target.id == 'grey') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id == 'white') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id == 'blue') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id == 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id == 'purple') {
-      body.style.backgroundColor = e.target.id;
-    }
-  });
-});
-
-
-```
-
-## Project 2
-
-```Javascript
 const form = document.querySelector('form');
 
 // This usecase will give you empty :
@@ -80,7 +34,3 @@ form.addEventListener('submit', function (e) {
     }
   }
 });
-
-
-
-```
